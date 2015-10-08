7cb4974ae36292da520cd5bcacd034c417e662ea
Marek Palewski <m.palewski@gmail.com>
2015-10-08 22:06:38 +0200

    Add some shapes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,12 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 %w(John Ben Dan).each do |user_name|
-  u = User.create(name: user_name)
+  user = User.create(name: user_name)
   %w(white-board green-table).each do |board_name|
-    u.diagrams << Diagram.create(name: board_name)
+    diagram = Diagram.create(name: board_name)
+    diagram.shapes << Circle.create(label: 'red-circle')
+    diagram.shapes << Rectangle.create(label: 'yellow-rectangle')
+    user.diagrams << diagram
   end
 end
 
