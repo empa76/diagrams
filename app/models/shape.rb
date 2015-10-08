cdf9e72bdcb0d151b78f866aa6e9a03fbb4f269f
Marek Palewski <m.palewski@gmail.com>
2015-10-08 22:04:29 +0200

    Extend shape model

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -1,5 +1,14 @@
 class Shape < ActiveRecord::Base
+  after_initialize :set_defaults
   belongs_to :diagram
-  validates :kind, :label, presence: true
-  store :properties, accessors: %i(color), coder: JSON
+  validates :label, presence: true
+  validates :kind, inclusion: { in: %w(circle rectangle) }, presence: true
+  store :properties, accessors: %i(color center_x center_y), coder: JSON
+
+  private
+
+  def set_defaults
+    self.center_x = 0
+    self.center_y = 0
+  end
 end
