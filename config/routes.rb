e8a2e8c1c539e3e5eb5caa407315c31b8c3aa85e
Marek Palewski <m.palewski@gmail.com>
2015-10-08 22:05:54 +0200

    Add shapes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :users, only: [:index, :show] do
-        resources :diagrams
+        resources :diagrams do
+          resources :shapes
+        end
       end
     end
   end
