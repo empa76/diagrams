454997fda2209a2d2a7e8f22cd1efa09f6bc74da
Marek Palewski <m.palewski@gmail.com>
2015-10-11 11:47:54 +0200

    Update missing specs for Diagram and Share

--- spec/requests/api/v1/shapes_spec.rb
+++ spec/requests/api/v1/shapes_spec.rb
@@ -1,30 +1,31 @@
 require 'rails_helper'
 
-describe 'api/v1/diagrams' do
+describe 'api/v1/shapes' do
 
   before do
-    FactoryGirl.create_list(:diagram, 2)
+    FactoryGirl.create_list(:diagram_with_shapes, 2)
   end
 
   let(:diagram) { Diagram.first }
+  let(:shape) { diagram.shapes.first }
 
   describe :index do
     before do
-      get api_v1_diagrams_path
+      get api_v1_diagram_shapes_path(diagram_id: diagram)
     end
 
     it 'response is success' do
       expect_response_to_be_success
     end
 
-    it "get all diagrams" do
-      expect(response_json["diagrams"]).to have(2).items
+    it "get all shapes" do
+      expect(response_json["shapes"]).to have(2).items
     end
   end
 
   describe :show do
     before do
-      get api_v1_diagram_path(diagram)
+      get api_v1_diagram_shape_path(diagram_id: diagram, id: shape.id)
     end
 
     it 'response is success' do
@@ -32,23 +33,23 @@ describe 'api/v1/diagrams' do
     end
 
     it "returns correct JSON" do
-      serializer = DiagramSerializer.new(diagram)
+      serializer = ShapeSerializer.new(shape)
       receipt_serialized = JSON.parse(serializer.to_json)
       expect(response_json).to eql(receipt_serialized)
     end
   end
 
   describe :destroy do
-    it 'removes diagram from list' do
-      delete api_v1_diagram_path(diagram)
+    it 'removes shape from list' do
+      delete api_v1_diagram_shape_path(diagram_id: diagram, id: shape)
 
       expect_response_to_be_success
       expect(response_json).to be_empty
-      expect(Diagram.all).to have(1).item
+      expect(diagram.shapes.count).to eq(1)
     end
 
     it "returns error when record not found" do
-      delete api_v1_diagram_path(id: 0)
+      delete api_v1_diagram_shape_path(diagram_id: diagram, id: 0)
       expect_response_to_be_not_found
     end
   end
@@ -56,11 +57,11 @@ describe 'api/v1/diagrams' do
   describe :create do
     context "when is successfully created" do
       before do
-        post api_v1_diagrams_path(diagram: attributes_for(:diagram))
+        post api_v1_diagram_shapes_path(diagram_id: diagram, shape: attributes_for(:shape))
       end
 
       it "renders the JSON for the record just created" do
-        expect(Diagram.count).to eq(3)
+        expect(diagram.shapes.count).to eq(3)
       end
 
       it "response is not empty" do
@@ -73,31 +74,28 @@ describe 'api/v1/diagrams' do
     end
 
     context "when is not created at all" do
-      it "should respond with 422" do
-        post api_v1_diagrams_path(diagram: attributes_for(:diagram, name: nil))
-        expect_response_to_be_unprocessable
+      before do
+        post api_v1_diagram_shapes_path(diagram_id: diagram, shape: attributes_for(:shape, kind: :square))
       end
 
-      it "should respond with 400 when params are invalid" do
-        post api_v1_diagrams_path(diagram: {})
-        expect_response_to_be_bad_request
+      it "should respond with 422" do
+        expect_response_to_be_unprocessable
       end
     end
   end
 
   describe :update do
-    let(:diagram_to_update) { Diagram.first }
+    let(:shape_to_update) { Shape.last }
 
     context "when is successfully updated" do
-
-      let(:new_value) { "my-new-shiny-diagram" }
+      let(:new_value) { "my-new-shiny-shape" }
 
       before do
-        put api_v1_diagram_path(diagram_to_update, diagram: { name: new_value })
+        put api_v1_diagram_shape_path(diagram_id: shape_to_update.diagram, id: shape_to_update, shape: { label: new_value })
       end
 
       it "renders the JSON for the record just updated" do
-        expect(response_json['diagram']['name']).to eql(new_value)
+        expect(response_json['shape']['label']).to eql(new_value)
       end
 
       it "should respond with 200" do
@@ -111,7 +109,7 @@ describe 'api/v1/diagrams' do
 
     context "when is not updated at all" do
       before do
-        put api_v1_diagram_path(diagram_to_update, diagram: { name: nil })
+        put api_v1_diagram_shape_path(diagram_id: shape_to_update.diagram, id: shape_to_update, shape: { label: nil })
       end
 
       it "should respond with 422" do
